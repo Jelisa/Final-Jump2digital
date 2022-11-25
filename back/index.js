d560b7ab35818fb37ff2a9f863e425a45eeb034a
Alexandre Cumplido Bou <acumpbo@gmail.com>
2022-11-25 14:36:26 +0100

    Add cors

--- back/index.js
+++ back/index.js
@@ -1,3 +1,4 @@
+const cors = require('cors')
 const express = require('express');
 const {router} = require('./routes/routes.js')
 const { join } = require('path');
@@ -5,7 +6,7 @@ const { join } = require('path');
 const PORT = process.env.PORT || 4000;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json())
 
 app.use("/api/v1/",router);
