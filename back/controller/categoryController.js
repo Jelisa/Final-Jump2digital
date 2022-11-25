8706b1bf1b27d5555aee22c452035d95b97800cb
German <gerfersie@gmail.com>
2022-11-25 14:26:43 +0100

    Patata power

--- back/controller/categoryController.js
+++ back/controller/categoryController.js
@@ -1,10 +1,19 @@
-const categoria_info = require("../data/categoria.json")
+const categoria_info = require("../data/categoria1.json")
 const respuestas = require("../services/responseModule")
 
 const categoryInfoController = (req,res)=>{
     try{
             const data = categoria_info
-            respuestas.success(req,res,data)
+            const longitud = Object.keys(data)
+
+            const patata = longitud.map(elemento => {
+                data[elemento].id = elemento
+                return data[elemento]
+            })
+
+            console.log(patata);
+        
+            respuestas.success(req,res,patata)
     }
     catch {
         respuestas.error(req,res,"error de servidor")
